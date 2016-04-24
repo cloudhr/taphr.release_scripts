307ab513fd12d90bdd1e5efa8e6e4a9532160989
Larry Eliemenye <denachural@gmail.com>
2016-04-24 22:04:25 +0100

    testing git

--- release_to_aws.sh
+++ release_to_aws.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-git
+
+# Variables
+#---------------------
+
+AWS_ACCESS_KEY=${1}
+AWS_SECRET_KEY=${2}
+
+echo "check amamzon elastic beanstalk version"
+eb --version
+
+echo "Authenticating with Keys: $AWS_ACCESS_KEY and $AWS_SECRET_KEY"
