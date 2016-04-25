6c08af2358dc6905a4c0b21d3195fad39a66ef78
Larry Eliemenye <denachural@gmail.com>
2016-04-25 08:37:36 +0100

    testing git

--- release_to_aws.sh
+++ release_to_aws.sh
@@ -8,7 +8,7 @@ AWS_SECRET_KEY=${2}
 
 echo "check amamzon elastic beanstalk version"
 eb --version
-
+cd
 echo "Authenticating AWS with Keys: $AWS_ACCESS_KEY and $AWS_SECRET_KEY"
 
 echo "Creating project Archive..."
