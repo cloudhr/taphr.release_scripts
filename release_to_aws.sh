9256ffe71dd6c9181bf39ed8880537ccf8b27e49
Larry Eliemenye <denachural@gmail.com>
2016-04-26 04:49:03 +0100

    testing git

--- release_to_aws.sh
+++ release_to_aws.sh
@@ -8,9 +8,12 @@ AWS_SECRET_KEY=${2}
 APP_DIRECTORY="/var/lib/go-agent/pipelines/taphr-api-server/sources"
 echo "check amamzon elastic beanstalk version"
 eb --version
-cd $APP_DIRECTORY
-echo "Authenticating AWS with Keys: $AWS_ACCESS_KEY and $AWS_SECRET_KEY"
 
-echo "Creating project Archive..."
+#chnage directory to sources
+cd $APP_DIRECTORY
+
+#deploys to amazon default branch
 echo "Deploying to amazon..."
+eb create taphr-dev
+eb use taphr-dev
 eb deploy
