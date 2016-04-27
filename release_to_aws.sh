76024ff18cf6de3fc08cfca4674eee94a3f8a366
Larry Eliemenye <denachural@gmail.com>
2016-04-27 05:44:24 +0100

    Added env name as a parameter to allow a much more flexible deployments

--- release_to_aws.sh
+++ release_to_aws.sh
@@ -5,15 +5,16 @@
 
 AWS_ACCESS_KEY=${1}
 AWS_SECRET_KEY=${2}
+AWS_ENV_NAME=${3}
+
 APP_DIRECTORY="/var/lib/go-agent/pipelines/taphr-api-server/sources"
 echo "check amamzon elastic beanstalk version"
 eb --version
 
-#chnage directory to sources
+#change directory to application sources
 cd $APP_DIRECTORY
 
 #deploys to amazon default branch
-echo "Deploying to amazon..."
-eb create taphr-dev
-eb use taphr-dev
+echo "Deploying to amazon env : $AWS_ENV_NAME"
+eb use $AWS_ENV_NAME
 eb deploy
